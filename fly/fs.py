b8969ab833056715e1678bfa6fac62c144f30249
tatsuya <tatsuya4649developer@gmail.com>
2021-08-26 23:12:57 +0900

    pyfly fs added

---
+++ fly/fs.py
@@ -0,0 +1,27 @@
+import os
+
+class FlyFs:
+	def __init__(self):
+		self._mounts = list()
+	
+	@property
+	def mounts(self):
+		return self._mounts
+
+	def mount(self, path):
+		if not isinstance(path, str):
+			raise TypeError(
+				"path must be str type."
+			)
+
+		abspath = os.path.abspath(path)
+		if not os.path.isdir(abspath):
+			raise ValueError(
+				f"invalid path \"{path}\"."
+			)
+		self._mounts.append(abspath)
+
+if __name__ == "__main__":
+	fs = FlyFs()
+	fs.mount(".")
+	print(fs.mounts)
