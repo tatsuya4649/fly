a063a5aa9302a2ec5820bb6c017239eb3ede679d
tatsuya.s <57025585+tatsuya4649@users.noreply.github.com>
2021-09-14 16:53:18 +0900

    accept encoding parser added

--- encode.h
+++ encode.h
@@ -8,10 +8,11 @@
 #include "util.h"
 
 #define FLY_ENCODE_TYPE(x)		{ fly_ ## x, #x }
-#define FLY_ENCODE_NULL			{ 0, NULL }
+#define FLY_ENCODE_NULL			{ -1, NULL }
 #define FLY_ENCODE_END(e)		((e)->name == NULL)
 #define FLY_ENCODE_ASTERISK		{ fly_asterisk, "*" }
-enum fly_encoding{
+#define FLY_ACCEPT_ENCODING_HEADER		"Accept-Encoding"
+enum __fly_encoding_type{
 	fly_gzip,
 	fly_compress,
 	fly_deflate,
@@ -19,7 +20,7 @@ enum fly_encoding{
 	fly_br,
 	fly_asterisk,
 };
-typedef enum fly_encoding fly_encoding_e;
+typedef enum __fly_encoding_type fly_encoding_e;
 typedef char fly_encname_t;
 typedef Bytef fly_encbuf_t;
 
@@ -29,16 +30,28 @@ struct fly_encoding_type{
 };
 typedef struct fly_encoding_type fly_encoding_type_t;
 
-fly_encoding_type_t *fly_encode_from_type(fly_encoding_e type);
-fly_encoding_type_t *fly_encode_from_name(fly_encname_t *name);
+fly_encoding_type_t *fly_encoding_from_type(fly_encoding_e);
+fly_encoding_type_t *fly_encoding_from_name(fly_encname_t *name);
 fly_encname_t *fly_encname_from_type(fly_encoding_e type);
 
+struct __fly_encoding{
+	fly_encoding_type_t		*type;
+	/* 0~100% */
+	int						quality_value;
+	struct __fly_encoding	*next;
+};
 
-/* TODO: encoding */
+struct fly_request;
+typedef struct fly_request fly_request_t;
+
 struct fly_encoding{
+	struct __fly_encoding		*accepts;
+	struct fly_request			*request;
+	/* acceptable quantity */
+	size_t						actqty;
+	fly_bit_t					use: 1;
 };
 typedef struct fly_encoding fly_encoding_t;
-#include "request.h"
 int fly_accept_encoding(fly_request_t *req);
 
 #define FLY_ENCODE_SUCCESS			1
@@ -56,5 +69,9 @@ int fly_deflate_encode(fly_encbuf_t *encbuf, size_t encbuflen, fly_encbuf_t *dec
 /* identity encode/decode */
 int fly_identify_decode(fly_encbuf_t *encbuf, size_t encbuflen, fly_encbuf_t *decbuf, size_t decbuflen);
 int fly_identify_encode(fly_encbuf_t *encbuf, size_t encbuflen, fly_encbuf_t *decbuf, size_t decbuflen);
+
+#define FLY_ENCNAME_MAXLEN			(20)
+/* 6 = int and point and 3 decimal places */
+#define FLY_ENVQVALUE_MAXLEN		(6)
 
 #endif
