1b33be24a60fc16f8356550a625f94b1f898ad6c
tatsuya.s <57025585+tatsuya4649@users.noreply.github.com>
2021-11-05 16:35:02 +0900

    ready of 1.0.0 release

--- setup.py
+++ setup.py
@@ -3,12 +3,13 @@ import subprocess
 import os
 import sys
 import re
+from glob import glob
 
 def __run(command_name):
     if not isinstance(command_name, list):
         raise TypeError("must be list type.")
 
-    res = subprocess.call(command_name, shell=True)
+    res = subprocess.call(command_name, shell=False)
     if res != 0:
         raise RuntimeError("failure to make libfly.so")
 
@@ -16,15 +17,20 @@ def version_from_init():
     with open("./fly/__init__.py", "r") as f:
         lines = f.readlines()
         for line in lines:
-            result = re.match('\s*__version__\s*=\s*"(\d.\d.\d)"', line)
+            result = re.match('\s*__version__\s*=\s*"(\d+.\d+.\d+)"', line)
             if result is not None:
                 return result.group(1)
     raise RuntimeError("not found version in fly/__init__.py")
 
+def get_packages(package):
+    return [
+        dirpath
+        for dirpath, dirnames, filenames in os.walk(package)
+        if os.path.exists(os.path.join(dirpath, "__init__.py"))
+    ]
 """
 make fly library
 """
-print("make libfly.so")
 __run(["./configure"])
 __run(["make"])
 __run(["make", "install"])
@@ -36,11 +42,11 @@ if os.getenv("DEBUG") is not None:
     extra_compile_args.append("-O0")
 
 server = Extension(
-	name="_fly_server",
+	name="fly._fly_server",
 	sources=["src/pyserver.c"],
-	library_dirs=["./lib"],
+	library_dirs=["./fly/lib"],
 	libraries=["fly"],
-	runtime_library_dirs=["./lib"],
+	runtime_library_dirs=["lib"],
     extra_compile_args = extra_compile_args,
     define_macros = macros,
 )
@@ -52,4 +58,10 @@ setup(
 	ext_modules = [
 		server,
 	],
+    packages = get_packages("fly"),
+    include_package_data=True,
+    package_data = {
+        "fly": glob('fly/lib/*'),
+    },
+    license_files = ('LICENSE'),
 )
