c82161d0727145cb58fb0650f7661d5425ced5d9
tatsuya.s <tatsuya4946deverloper@gmail.com>
2021-11-14 09:10:55 +0900

    daemon log bug fix

--- tests/test_daemon.py
+++ tests/test_daemon.py
@@ -1,7 +1,25 @@
 import pytest
 import asyncio
-
+import os
+import signal
 
 @pytest.mark.asyncio
 async def test_daemon():
     await asyncio.create_subprocess_shell("python -m fly tests/fly_test.py -c tests/http_test.conf --daemon")
+    await asyncio.sleep(2)
+
+    process = await asyncio.create_subprocess_shell("lsof -i:1234 -Fp | sed -e 's/^p//'", stdout = asyncio.subprocess.PIPE)
+    await process.wait()
+    __out, __err = await process.communicate()
+    print("\n")
+    pro = __out.decode("utf-8")
+    print(pro.split('\n'))
+
+    j=0
+    for i in pro.split('\n'):
+        if (len(i) > 0):
+            j+=1
+            os.kill(int(i), signal.SIGINT)
+
+    assert(j == 2)
+
