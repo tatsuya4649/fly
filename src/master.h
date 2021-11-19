44304fb068edf242e74da93010fc7afe93597f97
tatsuya.s <57025585+tatsuya4649@users.noreply.github.com>
2021-11-19 20:18:52 +0900

    add debug reloader.

--- src/master.h
+++ src/master.h
@@ -20,17 +20,29 @@
 enum fly_proc_type{
 	WORKER,
 };
+
 typedef enum fly_proc_type fly_proc_type;
 struct fly_master{
-	pid_t pid;
-	int req_workers;
-	int now_workers;
-	void (*worker_process)(fly_context_t *ctx, void *data);
-	struct fly_pool_manager *pool_manager;
-	struct fly_event_manager *event_manager;
+	pid_t						pid;
+	int							req_workers;
+	int 						now_workers;
+	void		(*worker_process)(fly_context_t *ctx, void *data);
+	struct fly_pool_manager		*pool_manager;
+	struct fly_event_manager	*event_manager;
 
-	struct fly_bllist workers;
-	fly_context_t *context;
+	struct fly_bllist			workers;
+	fly_context_t				*context;
+
+	struct fly_bllist			reload_filepath;
+	size_t						reload_pathcount;
+	fly_bit_t					detect_reload:1;
+};
+
+struct fly_watch_path{
+	int							wd;
+	const char					*path;
+	struct fly_bllist			blelem;
+	fly_bit_t					configure:1;
 };
 #define FLY_MASTER_POOL_SIZE				100
 typedef struct fly_master fly_master_t;
@@ -48,7 +60,11 @@ fly_context_t *fly_master_release_except_context(fly_master_t *master);
 /*
  * waiting for signal foever. wait or end.
  */
-__direct_log void fly_master_process(fly_master_t *master);
+
+#define FLY_MASTER_CONTINUE				0
+#define FLY_MASTER_SIGNAL_END			1
+#define FLY_MASTER_RELOAD				2
+__direct_log int fly_master_process(fly_master_t *master);
 void fly_master_worker_spawn(fly_master_t *master, void (*proc)(fly_context_t *, void *));
 
 #define __FLY_DEVNULL		("/dev/null")
@@ -62,5 +78,6 @@ void fly_master_worker_spawn(fly_master_t *master, void (*proc)(fly_context_t *,
 
 #define FLY_CREATE_PIDFILE					"FLY_CREATE_PIDFILE"
 bool fly_is_create_pidfile(void);
+void fly_master_setreload(fly_master_t *master, const char *reload_filepath, bool configure);
 
 #endif
